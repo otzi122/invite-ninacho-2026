70b580c9aad8694f1789a20c21e233b511ae062f
Luis Pinto <otzi122@gmail.com>
2025-05-02 10:55:46 -0400

    feat: add pause

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Control de sonido del video
 const video = document.getElementById('bgVideo');
+const videoContainer = document.querySelector('.video-background');
 const soundToggle = document.querySelector('.sound-toggle');
 const playToggle = document.querySelector('.play-toggle');
 
@@ -10,22 +10,17 @@ const bubblesContainer = document.querySelector('.bubbles');
 function createBubble() {
     const bubble = document.createElement('div');
     bubble.className = 'bubble';
-    
-    // Tamaño aleatorio
+
     const size = Math.random() * 60 + 20;
     bubble.style.width = `${size}px`;
     bubble.style.height = `${size}px`;
-    
-    // Posición horizontal aleatoria
+
     bubble.style.left = `${Math.random() * 100}%`;
-    
-    // Duración aleatoria
+
     const duration = Math.random() * 4 + 4;
     bubble.style.animation = `float ${duration}s infinite`;
-    
     bubblesContainer.appendChild(bubble);
-    
-    // Eliminar la burbuja después de la animación
+
     setTimeout(() => {
         bubble.remove();
     }, duration * 1000);
@@ -44,7 +39,6 @@ menuTrigger.addEventListener('click', () => {
 
 document.addEventListener('click', (e) => {
     if (!sidebar.contains(e.target) && !menuTrigger.contains(e.target)) {
-        video.muted = !video.muted;
         sidebar.classList.remove('active');
         menuTrigger.querySelector('.open').classList.remove('hidden');
         menuTrigger.querySelector('.close').classList.add('hidden');
@@ -52,6 +46,8 @@ document.addEventListener('click', (e) => {
 });
 
 soundToggle.addEventListener('click', () => {
+
+    console.log(video.muted);
     video.muted = !video.muted;
     soundToggle.innerHTML = video.muted
         ? '<i class="fa-solid fa-volume-xmark"></i>'
@@ -64,10 +60,12 @@ playToggle.addEventListener('click', () => {
         video.play();
         playToggle.innerHTML = '<i class="fa-solid fa-pause"></i>';
         playToggle.classList.remove('paused');
+        videoContainer.classList.remove('video-paused');
         return;
     }
 
     video.pause();
     playToggle.innerHTML = '<i class="fa-solid fa-play"></i>';
     playToggle.classList.add('paused');
+    videoContainer.classList.add('video-paused');
 });
